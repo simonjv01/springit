6e9d7138e9e835e5020242028a2476fb44a3ac6c
Simon Vargas <sjvargas01@gmail.com>
2024-02-04 19:54:46 -0500

    change test

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -8,7 +8,7 @@ public class SpringitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("Welcome to Springit! A Reddit clone!");
+        System.out.println("Welcome to Springit!");
     }
 
 }
