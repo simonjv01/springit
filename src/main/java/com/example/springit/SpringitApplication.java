1cc2f55042eda9b441f084ae1a3c2f7c6c952007
Simon Vargas <sjvargas01@gmail.com>
2024-02-04 19:48:18 -0500

    minor change

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringitApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(
-                SpringitApplication.class, args);
-        System.out.println("Welcome to Springit!");
+        SpringApplication.run(SpringitApplication.class, args);
+        System.out.println("Welcome to Springit! A Reddit clone!");
     }
 
 }
