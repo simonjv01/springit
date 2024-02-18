324818f9977031dd81cf1a9e2e070de9bbfbcbac
Simon Vargas <sjvargas01@gmail.com>
2024-02-17 22:59:49 -0500

    created models Link, Comment, and Vote

---
+++ src/main/java/com/example/springit/model/Vote.java
@@ -0,0 +1,54 @@
+package com.example.springit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+@Entity
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+    // user
+
+    // link
+
+
+    public Vote() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote vote1 = (Vote) o;
+        return vote == vote1.vote && Objects.equals(id, vote1.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, vote);
+    }
+
+
+}
