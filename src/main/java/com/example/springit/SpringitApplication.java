afc47f388adbec29fc08f732de90096ad1d04608
Simon Vargas <sjvargas01@gmail.com>
2024-02-17 22:15:31 -0500

    updated files

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -1,14 +1,19 @@
 package com.example.springit;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringitApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
-        System.out.println("Welcome to Springit!");
+        System.out.println("Welcom to Springit!");
     }
+    
+
 
 }
