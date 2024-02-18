7f76775e9d905fc8499b26d56ad3383542c3db8b
Simon Vargas <sjvargas01@gmail.com>
2024-02-17 22:22:47 -0500

    rebase

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -1,4 +1,7 @@
 package com.example.springit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,12 +11,26 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringitApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
         System.out.println("Welcom to Springit!");
+
     }
     
+
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            log.error("CommandLineRunner.run();");
+            log.warn("CommandLineRunner.run();");
+            log.info("CommandLineRunner.run();");
+            log.debug("CommandLineRunner.run();");
+            log.trace("CommandLineRunner.run();");
+        };
+    }
 
 
 }
