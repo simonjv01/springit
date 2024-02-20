4a2a4d8c1df2c1d906f7831d35210c300314b4e2
Simon Vargas <sjvargas01@gmail.com>
2024-02-19 20:55:11 -0500

    created repositories for Link and Comment

---
+++ src/main/java/com/example/springit/repository/LinkRepository.java
@@ -0,0 +1,8 @@
+package com.example.springit.repository;
+
+import com.example.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+    
+}
